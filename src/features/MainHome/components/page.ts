0e6a612fb549a47066cff75d065226092013e745
ahmed <ahmedsolliman28@gmail.com>
2024-10-11 14:38:07 +0300

    lasted Updated

--- src/features/MainHome/components/page.ts
+++ src/features/MainHome/components/page.ts
@@ -1,2 +1,4 @@
 export * from './Activities/page';
 export * from './Hero/page';
+export * from './PackagesSection/page'
+export * from './Blog/page';
--- src/features/MainHome/page.tsx
+++ src/features/MainHome/page.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
-import { Hero } from './components/page'
+import { Blog, Hero } from './components/page'
 import Activities from './components/Activities/page'
+import PackagesSection from './components/PackagesSection/page'
 
 export const MainHome = () => {
   return (
     <div>
       <Hero/>
       <Activities/>
+      <PackagesSection/>
+      <Blog/>
     </div>
   )
 }
