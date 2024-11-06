3bf2261c0cbaa7b8f53903d729e98a32d5929b8a
ahmed <ahmedsolliman28@gmail.com>
2024-11-06 17:51:53 +0200

    Booking Succ/ paymentMethod

--- src/features/Booking/components/PaymentMethod/page.tsx
+++ src/features/Booking/components/PaymentMethod/page.tsx
@@ -1,20 +1,9 @@
+"use client";
 import React from "react";
 import Image from "next/image";
 import {
-  arrowDown,
-  check,
+  eye,
   ships,
-  smallEma,
-  checkBlack,
-  jetski,
-  jetCar,
-  flyBorad,
-  parasilling,
-  extra,
-  packages,
-  bananaBoat,
-  donutBoat,
-  leftArrow,
   leftSlide,
   rightArrow,
 } from "../../../../../public/images/page";
@@ -23,162 +12,78 @@ export const PaymentMethod = () => {
   return (
     <div className="  lg:pl-[167px] lg:pr-[100px] xs:px-[20px] py-8 w-full  ">
       <h1 className="text-3xl font-bold mb-6 text-primary">
-        Reservation Details
+        Payment Method
       </h1>
 
       <div className="grid grid-cols-1 xl:grid-cols-2 lg:grid-cols-2 md:grid-cols-2 gap-10 ">
         {/* Booking Form */}
-        <div className="lg:w-3/4 xs:w-full  ">
-          <div className="grid grid-cols-1 sm:grid-cols-2 gap-5 my-7">
+        <div className="lg:w-3/4 xs:w-full relative  ">
+          <div className="grid grid-cols-1 lg:grid-cols-1 gap-4 my-7">
             <label
-              htmlFor="firstname"
+              htmlFor="nameOnCard"
               className="relative block overflow-hidden border-b border-h4Color bg-transparent pt-3 focus-within:border-blue-600"
             >
               <input
                 type="text"
-                id="firstname"
-                placeholder="Email"
+                id="nameOnCard"
+                placeholder="Name on Card"
                 className="peer h-8 w-full border-none bg-transparent p-0 placeholder-transparent focus:border-transparent focus:outline-none focus:ring-0 sm:text-sm"
               />
               <span className="absolute start-0 top-2 -translate-y-1/2 text-xs text-h4Color text-h6color transition-all peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-sm peer-focus:top-2 peer-focus:text-xs">
-                First Name
+                Name on Card
+              </span>
+            </label>
+          </div>
+          <div className="grid grid-cols-1 sm:grid-cols-2 gap-5 my-7">
+            <label
+              htmlFor="expiryDate"
+              className="relative block overflow-hidden border-b border-h4Color bg-transparent pt-3 focus-within:border-blue-600"
+            >
+              <input
+                type="text"
+                id="expiryDate"
+                placeholder="Expiry Date"
+                className="peer h-8 w-full border-none bg-transparent p-0 placeholder-transparent focus:border-transparent focus:outline-none focus:ring-0 sm:text-sm"
+              />
+              <span className="absolute start-0 top-2 -translate-y-1/2 text-xs text-h4Color text-h6color transition-all peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-sm peer-focus:top-2 peer-focus:text-xs">
+                Expiry Date
               </span>
             </label>
 
             <label
-              htmlFor="lastName"
+              htmlFor="cvv"
               className="relative block overflow-hidden border-b border-h4Color bg-transparent pt-3 focus-within:border-blue-600"
             >
               <input
                 type="text"
-                id="lastName"
-                placeholder="Name"
+                id="cvv"
+                placeholder="CVV"
                 className="peer h-8 w-full border-none bg-transparent p-0 placeholder-transparent focus:border-transparent focus:outline-none focus:ring-0 sm:text-sm"
               />
               <span className="absolute start-0 top-2 -translate-y-1/2 text-xs text-h4Color transition-all peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-sm peer-focus:top-2 peer-focus:text-xs">
-                Last Name
+                CVV
               </span>
-            </label>
-          </div>
-          <div className="grid grid-cols-1 lg:grid-cols-1 gap-4 my-7">
-            <label
-              htmlFor="email"
-              className="relative block overflow-hidden border-b border-h4Color bg-transparent pt-3 focus-within:border-blue-600"
-            >
-              <input
-                type="email"
-                id="email"
-                placeholder="Email"
-                className="peer h-8 w-full border-none bg-transparent p-0 placeholder-transparent focus:border-transparent focus:outline-none focus:ring-0 sm:text-sm"
+              <Image
+                src={eye}
+                alt="eye"
+                className="w-4 h-4 absolute right-0 top-1/2 transform -translate-y-1/2"
               />
-              <span className="absolute start-0 top-2 -translate-y-1/2 text-xs text-h4Color text-h6color transition-all peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-sm peer-focus:top-2 peer-focus:text-xs">
-                Email
-              </span>
             </label>
           </div>
-          <div className="grid grid-cols-1 lg:grid-cols-1 gap-4 my-7">
-            <label
-              htmlFor="phone"
-              className="relative block overflow-hidden border-b border-h4Color bg-transparent pt-3 focus-within:border-blue-600"
-            >
-              <div className="flex items-center">
-                <div className="flex items-center gap-2 mr-2">
-                  <Image
-                    src={smallEma}
-                    alt="Country flag"
-                    className="w-6 h-4 object-cover"
-                  />
-                  <Image
-                    src={arrowDown}
-                    alt="Select country"
-                    className="w-4 h-4"
-                  />
-                </div>
-                <span className="text-h6Color">+971</span>
-                <input
-                  type="number"
-                  id="phone"
-                  placeholder="Phone"
-                  className="peer h-8 w-full border-none bg-transparent p-0 placeholder-transparent focus:border-transparent focus:outline-none focus:ring-0 sm:text-sm"
-                />
-              </div>
-              <span className="absolute start-0 top-2 -translate-y-1/2 text-xs text-h4Color text-h6color transition-all peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-sm peer-focus:top-2 peer-focus:text-xs">
-                Phone
-              </span>
-            </label>
-          </div>
-          <div className="border-b border-h6Color my-20"></div>{" "}
-          <h1 className="text-primary text-xl font-bold my-7">
-            Additional Services
-          </h1>
-          <div className="border rounded-lg p-4 mt-4 border-h1Color flex items-center justify-between">
-            <div className="flex items-center gap-2">
-              <Image src={check} alt="Check" />
-              <span className="text-primary font-medium mx-3">Photography</span>
-            </div>
-            <span className="text-primary border  bg-h5Color rounded-full px-2 ">
-              1200 AED
-            </span>
-          </div>
-          <div className="border rounded-lg p-4 mt-4 border-h1Color  items-center justify-between">
-            <div className="flex justify-between">
-              <div className="flex items-center gap-2">
-                <Image src={checkBlack} alt="Check" />
-                <span className="text-primary font-medium mx-3">
-                  WaterSport
-                </span>
-              </div>
-              <span className="text-primary border bg-h5Color rounded-full px-2">
-                1200 AED
-              </span>
-            </div>
 
-            <div className="flex items-center justify-center my-4 space-x-8">
-              <div className="flex flex-col items-center gap-2">
-                <Image src={jetski} alt="Check" />
-                <div className="text-center">Jet Ski</div>
-              </div>
-              <div className="flex flex-col items-center gap-2">
-                <Image src={jetCar} alt="Check" />
-                <div className="text-center">Jet Car</div>
-              </div>
-              <div className="flex flex-col items-center gap-2">
-                <Image src={flyBorad} alt="Check" />
-                <div className="text-center">Fly Board</div>
-              </div>
-              <div className="flex flex-col items-center gap-2">
-                <Image src={parasilling} alt="Check" />
-                <div className="text-center">Parasilling</div>
-              </div>
-            </div>
-            <div className="flex items-center justify-center my-3 space-x-8">
-              <div className="flex flex-col items-center gap-2">
-                <Image src={bananaBoat} alt="Check" />
-                <div className="text-center">Banana Boat</div>
-              </div>
-              <div className="flex flex-col items-center gap-2">
-                <Image src={donutBoat} alt="Check" />
-                <div className="text-center">Donut Boat</div>
-              </div>
-              <div className="flex flex-col items-center gap-2">
-                <Image src={packages} alt="Check" />
-                <div className="text-center">Packages</div>
-              </div>
-              <div className="flex flex-col items-center gap-2">
-                <Image src={extra} alt="Check" />
-                <div className="text-center">Extra</div>
-              </div>
-            </div>
-            <div className="border-b border-h4Color mt-10"></div>
-
-            <div className="flex items-center space-x-4 font-medium my-4 mx-10 ">
-              <span className="text-primary">Guests:</span>
-              <span className="text-h6Color">1</span>
-              <Image src={arrowDown} alt="Arrow" className="text-primary" />
-            </div>
+          <div className="">
+            <h2 className="text-[15px]  font-bold text-h6Color">
+              Free cancellation before 9:00 AM on October 9 (tour local time)
+            </h2>
+            <p className="text-h6Color text-[12px] my-4">
+              By clicking "Complete Booking", you acknowledge that you have read
+              and are bound by EnjoySpot's Terms & Privacy and Cookies
+              Statement; Viator's Terms; plus the tour operator's rules &
+              regulations (see listing for more details).
+            </p>
           </div>
-          <div className="my-7 flex justify-between font-bold gap-4">
-            <button className="bg-white text-primary px-4 py-2 rounded-lg w-[140px] border flex items-center justify-center gap-2">
+          <div className="my-7 flex justify-between  font-bold gap-4 ">
+            <button className=" absolute bottom-0 left-0 bg-white text-primary px-4 py-2 rounded-lg w-[140px] border flex items-center justify-center gap-2">
               <Image
                 src={leftSlide}
                 alt="Arrow"
@@ -186,7 +91,7 @@ export const PaymentMethod = () => {
               />
               Back
             </button>
-            <button className="bg-primary text-white px-4 py-2 rounded-lg w-[140px] border flex items-center justify-center gap-2">
+            <button className="bg-primary absolute bottom-0 right-0 text-white px-4 py-2 rounded-lg w-[140px] border flex items-center justify-center gap-2">
               Next
               <Image
                 src={rightArrow}
@@ -196,6 +101,7 @@ export const PaymentMethod = () => {
             </button>
           </div>
         </div>
+
         {/* Booking Summary */}
         <div className="bg-white p-4 md:p-6 w-full border-l-2 border-l-h4Color ">
           <Image
@@ -248,3 +154,5 @@ export const PaymentMethod = () => {
     </div>
   );
 };
+
+export default PaymentMethod;
--- src/features/Booking/page.tsx
+++ src/features/Booking/page.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
+// import { ReservationDetails } from './components/ReservationDetails/page';
 import { PaymentMethod } from './components/PaymentMethod/page';
-
+import SuccessfulReservation from './components/SuccessfulReservation/page';
 export const Booking = () => {
   return (
     <div>
+    {/* <ReservationDetails/> */}
     <PaymentMethod/>
+    {/* <SuccessfulReservation/> */}
       
     </div>
   )
