566239c42ba4054ea36302923b75fa8fd6326742
ahmed <ahmedsolliman28@gmail.com>
2024-11-22 00:07:33 +0200

    pages

--- src/features/Transaction/components/NoBooking/page.tsx
+++ src/features/Transaction/components/NoBooking/page.tsx
@@ -1,10 +1,25 @@
+import { Button } from '@/components/page'
+import Image from 'next/image'
 import React from 'react'
+import { cuate } from '../../../../../public/images/page'
 
 export const NoBooking = () => {
   return (
-    <div>
-      <h1>No Booking</h1>
+    <>
+    <div
+      className="flex flex-col justify-center space-y-5 items-center mx-auto mt-20"
+      
+    >
+      <h1 className="font-Sans text-primary text-3xl text-center font-bold   ">
+        No booking yet!
+      </h1>
+      <h1 className="font-Sans text-primary text text-center ">
+        Start your first luxury adventure NOW
+      </h1>
+      <Button className=" mt-10 w-[340px]">Back to Home</Button>
+      <Image src={cuate} alt="netCon" />
     </div>
+  </>
   )
 }
 
