2f7e970d8dc117e62654b18830bad909ab603149
ahmed <ahmedsolliman28@gmail.com>
2024-11-29 15:09:57 +0200

    changes

--- src/components/CardDeals/page.tsx
+++ src/components/CardDeals/page.tsx
@@ -41,87 +41,89 @@ const CardDeals = ({
   console.log(attachmentsLoop);
 
   return (
-    <div className="relative max-w-[392px]  ">
+    <div className="relative max-w-[392px]   ">
       {/* Main image */}
       <Link href="" className="absolute right-3 top-3 z-10">
         <Image src={convert} width={40} height={40} alt="convert" />
       </Link>
 
-      <div className="absolute inset-0 bg-gradient-to-t from-primary to-transparent opacity-85 z-10 rounded-[20px]"></div>
+      <Link href={`/offer/${deal.id}`} className="relative">
+        <div className="absolute inset-0 bg-gradient-to-t from-primary to-transparent opacity-85 z-10 rounded-[20px]"></div>
 
-      {mediaAttachments.length > 0 ? (
-        <Image
-          alt="Activity Image"
-          src={`https://enjoyspot.premiumasp.net${mediaAttachments[0].attachmentPath}`}
-          width={392}
-          height={400}
-          className="w-full  object-cover rounded-[20px] h-[400px]"
-        />
-      ) : (
-        <div className="w-full h-48 bg-gray-200 flex items-center justify-center">
-          <p className="text-gray-500">No Image Available</p>
-        </div>
-      )}
-
-      {/* Gradient overlay */}
-
-      <div className="absolute left-[3%] bottom-[3%]  w-[110px] max-w-[110px]  z-50 rounded-[12px]  h-[124px] font-sans bg-highlight text-primary text-center border">
-        <div className="flex flex-col justify-center my-3">
-          {/* priceAfter */}
-          {priceAfter && (
-            <>
-              <h2 className="text-2xl font-bold ">{priceAfter}</h2>
-              <h5 className="text-xs ">AED/H</h5>
-            </>
-          )}
-          {/* priceBefore with line-through */}
-          <h2 className="text-2xl  font-bold  line-through text-h6Color ">
-            {deal.price}
-          </h2>
-          <h2 className="text-xs ">AED/H</h2>
-        </div>
-      </div>
-
-      <div className="absolute left-[30%] bottom-[1%]   z-50 rounded-[12px]  w-[70%] max-w-[70%]  h-[135px] text-white    ">
-        <div className="flex flex-col ">
-          {/* priceAfter */}
-          <div className="">
-            <h2 className="text-2xl   font-bold text-highlight ml-3">
-              {deal.name}
-            </h2>
-          </div>
-
-          <div className=" flex  my-3 ">
-            <h2 className="text-xs  mt-2  flex ml-3 ">
-              <Image src={seats} width={18} height={19} alt="seats" />{" "}
-              {deal.seats}
-            </h2>
-            <div className="border-r mx-3  bg-h6Color h-[15px] mt-3 "></div>
-            <h2 className="text-xs  mt-2 flex ">
-              <Image src={timer} width={18} height={19} alt="seats" />{" "}
-              {deal.durations}
-            </h2>
-          </div>
-
-          {/* priceBefore with line-through */}
-          <Button className=" bg-white text-primary       transition flex justify-center items-center w-[250px]   font-bold mt-1 py-2  mx-2">
-            <span>Book Now</span>
-          </Button>
-        </div>
-      </div>
-
-      <div className="absolute right-[8%] bottom-[28%]  z-10 ">
-        <h2 className="flex text-white text-xs">
-          {deal.rating}/5
+        {mediaAttachments.length > 0 ? (
           <Image
-            src={stars}
-            width={16}
-            height={17}
-            alt="stars"
-            className="mx-1"
+            alt="Activity Image"
+            src={`https://enjoyspot.premiumasp.net${mediaAttachments[0].attachmentPath}`}
+            width={392}
+            height={400}
+            className="w-full  object-cover rounded-[20px] h-[400px]"
           />
-        </h2>
-      </div>
+        ) : (
+          <div className="w-full h-48 bg-gray-200 flex items-center justify-center">
+            <p className="text-gray-500">No Image Available</p>
+          </div>
+        )}
+
+        {/* Gradient overlay */}
+
+        <div className="absolute left-[2%] bottom-[3%]  w-[90px] max-w-[110px]  z-50 rounded-[12px]  h-[124px] font-sans bg-highlight text-primary text-center border">
+          <div className="flex flex-col justify-center my-3">
+            {/* priceAfter */}
+            {priceAfter && (
+              <>
+                <h2 className="text-2xl font-bold ">{priceAfter}</h2>
+                <h5 className="text-xs ">AED/H</h5>
+              </>
+            )}
+            {/* priceBefore with line-through */}
+            <h2 className="text-2xl  font-bold  line-through text-h6Color ">
+              {deal.price}
+            </h2>
+            <h2 className="text-xs ">AED/H</h2>
+          </div>
+        </div>
+
+        <div className="absolute left-[33%] bottom-[1%]   z-50 rounded-[12px]  w-[70%] max-w-[70%]  h-[135px] text-white    ">
+          <div className="flex flex-col ">
+            {/* priceAfter */}
+            <div className="">
+              <h2 className="text-2xl   font-bold text-highlight ml-3">
+                {deal.name}
+              </h2>
+            </div>
+
+            <div className=" flex  my-3 ">
+              <h2 className="text-xs  mt-2  flex ml-3 ">
+                <Image src={seats} width={18} height={19} alt="seats" />{" "}
+                {deal.seats} Seats
+              </h2>
+              <div className="border-r mx-3  bg-h6Color h-[15px] mt-3 "></div>
+              <h2 className="text-xs  mt-2 flex ">
+                <Image src={timer} width={18} height={19} alt="seats" />{" "}
+                {deal.durations} Duration
+              </h2>
+            </div>
+
+            {/* priceBefore with line-through */}
+            <Button className=" bg-white text-primary  transition flex justify-center items-center   font-bold mt-1 py-2  mx-4 ">
+              <span>Book Now</span>
+            </Button>
+          </div>
+        </div>
+
+        <div className="absolute right-[8%] bottom-[28%]  z-10 ">
+          <h2 className="flex text-white text-xs">
+            {deal.rating}/5
+            <Image
+              src={stars}
+              width={16}
+              height={17}
+              alt="stars"
+              className="mx-1"
+            />
+          </h2>
+        </div>
+      </Link>
     </div>
   );
 };
--- src/features/SubCategories/components/BestDeals/page.tsx
+++ src/features/SubCategories/components/BestDeals/page.tsx
@@ -19,7 +19,9 @@ import { API_SERVICES_URLS } from "@/data/page";
 import CardYachts from "@/components/CardYachtsSub/page";
 
 const BestDeals = () => {
-  const { data, isLoading , error} = useSWRHook(API_SERVICES_URLS.GET_ALL_LISTINGS);
+  const { data, isLoading, error } = useSWRHook(
+    API_SERVICES_URLS.GET_ALL_LISTINGS
+  );
   const bestDeals = data?.isSuccess ? data?.data?.data : [];
   console.log(bestDeals);
   return (
@@ -188,12 +190,15 @@ const BestDeals = () => {
         />
       </section>
       {/* ---------------- {Deals}-------------- */}
-      <section className="grid grid-cols-1 xs:grid-cols-1  ss:grid-cols-1 custom:grid-cols-2 custom1:grid-cols-2 lg:grid-cols-3 gap-5 sm:gap-7 py-8 sm:py-12">
-        {bestDeals.map((deal:any) => (
-          <CardYachts key={deal.id} activity={deal}   />
-                    // <CardDeals key={deal.id} deal={deal} loading={isLoading} error={error} />
-
-                  ))}
+      <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 py-8 ">
+        {bestDeals.map((deal: any) => (
+          <CardDeals
+            key={deal.id}
+            deal={deal}
+            loading={isLoading}
+            error={error}
+          />
+        ))}
       </section>
 
       <section className="">
